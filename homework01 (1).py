19d27829624fb7cd797329decb35d746ff0372dc
Anatoly <107132732+Seven7897@users.noreply.github.com>
2022-09-04 09:08:54 +0300

    Delete 01 (1)

--- homework01 (1).py
+++
@@ -1,8 +0,0 @@
-day =  int (input ("Введите день недели :"))
-if day > 0 and day < 8 :
-    if day > 5 :
-        print('Выходной день') 
-    else :
-        print('Будний день') 
-else :
-    print('число' + day +  'не является днем недели')
