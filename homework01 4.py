fe0e07a742bf1c1147d0f2358ff9efcab12d9cc2
Anatoly Finchenko <finchenko777@gmail.com>
2022-09-04 08:22:40 +0300

    work4

---
+++ homework01 4.py
@@ -0,0 +1,9 @@
+number = int(input("Задайте номер четверти : "))
+if number == 1 :
+    print("x+ y+")
+if number == 2 :
+    print("x- y+")
+if number == 3 :
+    print("x- y-")
+if number == 4 :
+    print("x+ y-")
